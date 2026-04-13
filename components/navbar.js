5e71cd285066456ee3a4eb7bedc5ac6cb7a1f6f2
Camilo Castellanos <kml107@hotmail.com>
2026-04-13 17:14:25 -0500

    navbar component style added

--- components/navbar.js
+++ components/navbar.js
@@ -4,31 +4,12 @@ const navbar = `
 
     <a class="navbar-brand logo" href="#">coroto</a>
 
-    <div class="d-flex align-items-center order-lg-3 nav-icons">
-      <a href="#" class="position-relative">
-        <i class="bi bi-cart"></i>
-        <span class="cart-badge">2</span>
-      </a>
-
-      <div class="dropdown ms-3">
-        <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" data-bs-toggle="dropdown">
-          <i class="bi bi-person-circle"></i>
-        </a>
-
-        <ul class="dropdown-menu dropdown-menu-end">
-          <li><a class="dropdown-item" href="#">Mi perfil</a></li>
-          <li><a class="dropdown-item" href="#">Mis pedidos</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
-        </ul>
-      </div>
-    </div>
-
-    <button class="navbar-toggler order-lg-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse order-lg-1" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarNav">
+
       <ul class="navbar-nav mx-auto text-center">
         <li class="nav-item">
           <a class="nav-link active" href="#">Inicio</a>
@@ -40,6 +21,33 @@ const navbar = `
           <a class="nav-link" href="#">Productos</a>
         </li>
       </ul>
+
+      <ul class="navbar-nav ms-auto text-center">
+
+        <!-- Carrito -->
+        <li class="nav-item">
+          <a class="nav-link position-relative" href="#">
+            <i class="bi bi-cart"></i>
+            <span class="cart-badge">2</span>
+          </a>
+        </li>
+
+        <!-- Usuario -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+            <i class="bi bi-person-circle"></i> Cuenta
+          </a>
+
+          <ul class="dropdown-menu dropdown-menu-end text-center">
+            <li><a class="dropdown-item" href="#">Mi perfil</a></li>
+            <li><a class="dropdown-item" href="#">Mis pedidos</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+          </ul>
+        </li>
+
+      </ul>
+
     </div>
 
   </div>
@@ -52,4 +60,9 @@ document.addEventListener("DOMContentLoaded", () => {
     container.innerHTML = navbar;
   }
 });
+
+
+
+
+
 
