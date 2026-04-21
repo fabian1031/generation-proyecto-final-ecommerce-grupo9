b46d023884a4f0bf50940a98b4a144ddc1f10215
Camilo Castellanos <kml107@hotmail.com>
2026-04-21 11:10:21 -0500

    fix bugs

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,10 @@
 export class Product {
-    constructor({ id, name, brand ,price, stock, category, description, image }) {
+    constructor({ id, name, brand ,price, status, stock, category, description, image }) {
         this.id = id;
         this.name = name;
         this.brand = brand;
         this.price = price;
+        this.status = status;
         this.stock = stock;
         this.category = category;
         this.description = description;
--- styles/admin_dashboard.js
+++
@@ -1,3 +0,0 @@
-function ProductCrud() {
-    console.log("Hola");
-}
