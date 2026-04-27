67da143648c3f3923513b4ec5a0cde2f92d93759
Hernan_Vasquez <nachjvasquez@gmail.com>
2026-04-27 17:27:44 -0500

    add validation checkout to terms and conditions before pass to checkout

--- scripts/cartBadge.js
+++ scripts/cartBadge.js
@@ -2,11 +2,20 @@ import { cartService } from '../services/cartSevices.js';
 import { formatPrice } from '../services/utils.service.js';
 
 const container = document.getElementById('cartItems');
+const btnCheckout = document.getElementById('goCheckout');
+const checkboxTerms = document.getElementById('terms');
+
 
 function renderCart() {
     const cart = cartService.getCart();
 
     container.innerHTML = '';
+
+    if (cart.length === 0) {
+        document.getElementById('cartEmpty').classList.remove('d-none');
+    } else {
+        document.getElementById('cartEmpty').classList.add('d-none');
+    }
 
     cart.forEach(item => {
         const el = document.createElement('div');
@@ -25,6 +34,7 @@ function renderCart() {
                         <div>
                             <p class="small text-muted mb-1">${item.brand}</p>
                             <p class="fw-semibold mb-2">${item.name}</p>
+                            <p class="small text-muted mb-1">${item.description}</p>
                         </div>
 
                         <div class="d-flex justify-content-between mt-3">
@@ -47,7 +57,7 @@ function renderCart() {
             </div>
         `;
 
-        // eventos
+        // eventos increase & decrement
         el.querySelector('[data-action="increase"]').onclick = () => {
             cartService.updateQuantity(item.id, item.quantity + 1);
             renderCart();
@@ -63,7 +73,6 @@ function renderCart() {
         };
 
         el.querySelector('[data-action="delete"]').onclick = async () => {
-
             const result = await Swal.fire({
                 title: '¿Eliminar producto?',
                 text: `"${item.name}" será removido del carrito`,
@@ -88,12 +97,15 @@ function renderCart() {
                 showConfirmButton: false
             });
         };
+
         container.appendChild(el);
     });
 
     renderSummary();
+    updateCheckoutState();
 }
-//renderizamos aqui
+
+// resumen de compra
 function renderSummary() {
     const totalItems = cartService.getTotalItems();
     const totalPrice = cartService.getTotalPrice();
@@ -105,4 +117,43 @@ function renderSummary() {
     document.querySelector('#summary-total').textContent = formatPrice(totalPrice + iva);
 }
 
-document.addEventListener('DOMContentLoaded', renderCart);
+
+function updateCheckoutState() {
+    const cart = cartService.getCart();
+    const isValid = cart.length > 0 && checkboxTerms.checked;
+};
+
+// escuchar checkbox
+checkboxTerms.addEventListener('change', updateCheckoutState);
+
+btnCheckout.addEventListener('click', () => {
+    const cart = cartService.getCart();
+
+    if (cart.length === 0) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Carrito vacío',
+            text: 'Agrega productos antes de continuar'
+        });
+        return;
+    }
+
+    if (!checkboxTerms.checked) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Acepta los términos',
+            text: 'Debes aceptar los términos y condiciones para continuar'
+        });
+        return;
+    }
+
+    
+    window.location.href = './checkout.html';
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    renderCart();
+    updateCheckoutState();
+});
