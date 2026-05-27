4cd89193063011f372c6388d674f8bb868544154
Cristian Ceballos <ceballosmarincristiancamilo@gmail.com>
2026-05-27 15:16:05 -0500

    se modifica endpoin de payment

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -186,7 +186,7 @@ function bindCheckoutEvents() {
 
         try {
             const [res] = await Promise.all([
-                fetch('https://coroto.online', {
+                fetch('https://coroto.online/payment/payment.php', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
