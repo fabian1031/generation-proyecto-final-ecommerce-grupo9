a587a376250310c6cf5c72b137e7f0b6a3e265c8
Cristian Ceballos <ceballosmarincristiancamilo@gmail.com>
2026-05-27 13:11:10 -0500

    se ajusta rutas internas v7

--- services/utils.service.js
+++ services/utils.service.js
@@ -1,19 +1,3 @@
-export function getAppBase() {
-    const path = window.location.pathname;
-    const pagesMarker = '/pages/';
-    const pagesIdx = path.indexOf(pagesMarker);
-
-    if (pagesIdx !== -1) {
-        return path.slice(0, pagesIdx + 1);
-    }
-
-    return path.substring(0, path.lastIndexOf('/') + 1);
-}
-
-export function getPagesBase() {
-    return `${getAppBase()}pages/`;
-}
-
 export function formatPrice(price) {
     return new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP', maximumFractionDigits: 0 }).format(price);
 }
